4f2cccb6b49322ee82a6b7a77cc9fe1863ed2631
Eden Denis <edendenis@gmail.com>
2024-09-12 10:01:58 -0300

    updating readme

--- README.py
+++ README.py
@@ -27,21 +27,22 @@
 
 # 2. Certifique-se de que seu sistema esteja limpo e atualizado.
 # 
-#     2.1 Limpar o `cache` do gerenciador de pacotes APT. Especificamente, ele remove todos os arquivos de pacotes (`.deb`) baixados pelo APT e armazenados em `/var/cache/apt/archives/`. Digite o seguinte comando: `sudo apt clean` 
+#     2.1 **Limpar o `cache` do gerenciador de pacotes `apt`**: Especificamente, ele remove todos os arquivos de pacotes (`.deb`) baixados pelo `apt` e armazenados em `/var/cache/apt/archives/`. Digite o seguinte comando: `sudo apt clean` 
 #     
-#     2.2 Remover pacotes `.deb` antigos ou duplicados do cache local. É útil para liberar espaço, pois remove apenas os pacotes que não podem mais ser baixados (ou seja, versões antigas de pacotes que foram atualizados). Digite o seguinte comando: `sudo apt autoclean`
+#     2.2 **Remover pacotes `.deb` antigos ou duplicados do `cache` local**: É útil para liberar espaço, pois remove apenas os pacotes que não podem mais ser baixados (ou seja, versões antigas de pacotes que foram atualizados). Digite o seguinte comando: `sudo apt autoclean`
 # 
-#     2.3 Remover pacotes que foram automaticamente instalados para satisfazer as dependências de outros pacotes e que não são mais necessários. Digite o seguinte comando: `sudo apt autoremove -y`
+#     2.3 **Remover pacotes que foram automaticamente instalados para satisfazer as dependências de outros pacotes e que não são mais necessários**: Digite o seguinte comando: `sudo apt autoremove -y`
 # 
-#     2.4 Buscar as atualizações disponíveis para os pacotes que estão instalados em seu sistema. Digite o seguinte comando e pressione `Enter`: `sudo apt update -y`
+#     2.4 **Buscar as atualizações disponíveis para os pacotes que estão instalados em seu sistema**: Digite o seguinte comando e pressione `Enter`: `sudo apt update`
 # 
-#     2.5 Para ver a lista de pacotes a serem atualizados, digite o seguinte comando e pressione `Enter`:  `sudo apt list --upgradable`
+#     2.5 **Corrigir pacotes quebrados**: Isso atualizará a lista de pacotes disponíveis e tentará corrigir pacotes quebrados ou com dependências ausentes: `sudo apt --fix-broken install`
 # 
-#     2.6 Realmente atualizar os pacotes instalados para as suas versões mais recentes, com base na última vez que você executou `sudo apt update -y`. Digite o seguinte comando e pressione `Enter`: `sudo apt full-upgrade -y`
+#     2.6 **Limpar o `cache` do gerenciador de pacotes `apt`**: Especificamente, ele remove todos os arquivos de pacotes (`.deb`) baixados pelo `apt` e armazenados em `/var/cache/apt/archives/`. Digite o seguinte comando: `sudo apt clean` 
+#     
+#     2.7 **Para ver a lista de pacotes a serem atualizados**: Digite o seguinte comando e pressione `Enter`:  `sudo apt list --upgradable`
 # 
-#     2.7 Remover pacotes que foram automaticamente instalados para satisfazer as dependências de outros pacotes e que não são mais necessários. Digite o seguinte comando: `sudo apt autoremove -y`
-# 
-#     2.8 Remover pacotes `.deb` antigos ou duplicados do cache local. É útil para liberar espaço, pois remove apenas os pacotes que não podem mais ser baixados (ou seja, versões antigas de pacotes que foram atualizados). Digite o seguinte comando: `sudo apt autoclean`
+#     2.8 **Realmente atualizar os pacotes instalados para as suas versões mais recentes, com base na última vez que você executou `sudo apt update`**: Digite o seguinte comando e pressione `Enter`: `sudo apt full-upgrade -y`
+#     
 
 # Para verificar se alguém está tentando acessar seu computador no `Linux` através do `Terminal Emulator`, você pode usar várias ferramentas e comandos que fornecem informações sobre as conexões de rede, usuários logados e tentativas de login. Aqui estão algumas maneiras de fazer isso:
 # 
@@ -49,19 +50,23 @@
 # 
 # 2. **`w`:** Exibe quem está logado e o que estão fazendo: `w`
 # 
-# 3. **`last`:** Mostra um registro das últimas logins no sistema. Pode ser útil para ver tentativas recentes de acesso: `last`
+# 3. **`last`:** Mostra um registro das últimas _logins_ no sistema. Pode ser útil para ver tentativas recentes de acesso: `last`
 # 
 # 3. **`netstat`:** Fornece uma variedade de estatísticas de rede. Para ver conexões ativas, você pode usar: `netstat -tunapl`
 # 
 # -`-t` (TCP)
+# 
 # -`-u` (UDP)
+# 
 # -`-n` (Endereços como números)
+# 
 # -`-a` (Mostra todas as conexões)
+# 
 # -`-p` (Mostra o programa associado a cada conexão)
 # 
 # ### 1.1 Para usuários de sistemas que usam o `ss` em vez do `netstat`:
 # 
-# 1. **Verificar logs do sistema:** Logs como `/var/log/auth.log` (em sistemas baseados em Debian) podem fornecer informações sobre tentativas de login e outras atividades de autenticação: `sudo cat /var/log/auth.log`
+# 1. **Verificar _logs_ do sistema:** Logs como `/var/log/auth.log` (em sistemas baseados em Debian) podem fornecer informações sobre tentativas de login e outras atividades de autenticação: `sudo cat /var/log/auth.log`
 # 
 # É importante manter o sistema atualizado e seguir as melhores práticas de segurança, como usar firewalls, alterar senhas regularmente e usar autenticação de dois fatores, se possível, para melhor proteger seu sistema contra acessos não autorizados.
 
@@ -74,13 +79,13 @@
 # 2. Digite o seguinte comando e pressione `Enter`:
 # 
 #     ```
-#     **NÃO há.**
+#     NÃO há.
 #     ```
 #     
 
 # ## Referências
 # 
-# [1] OPENAI. ***Monitoramento de Acesso no Linux.*** Disponível em: <https://chat.openai.com/c/7772a507-88e7-46c8-ad7a-64a2d9011843> (texto adaptado). ChatGPT. Acessado em: 21/02/2024 12:00.
+# [1] OPENAI. ***Monitoramento de acesso no linux.*** Disponível em: <https://chat.openai.com/c/7772a507-88e7-46c8-ad7a-64a2d9011843> (texto adaptado). ChatGPT. Acessado em: 21/02/2024 12:00.
 # 
 # [2] OPENAI. ***Vs code: editor popular.*** Disponível em: <https://chat.openai.com/c/b640a25d-f8e3-4922-8a3b-ed74a2657e42> (texto adaptado). ChatGPT. Acessado em: 21/02/2024 12:00.
 # 
